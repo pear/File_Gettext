2e8e5700e3df62d30903da25b6ffe61cfb73e9ea
Michael Wallner <mike@php.net>
2005-03-30 18:26:48 +0000

    * new docblock header
    git-svn-id: https://svn.php.net/repository/pear/packages/File_Gettext/trunk@183188 c90b9560-bf6c-de11-be94-00142212c4b1

--- Gettext/PO.php
+++ Gettext/PO.php
@@ -1,27 +1,29 @@
 <?php
-// +----------------------------------------------------------------------+
-// | PEAR :: File :: Gettext :: PO                                        |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is available at http://www.php.net/license/3_0.txt              |
-// | If you did not receive a copy of the PHP license and are unable      |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2004 Michael Wallner <mike@iworks.at>                  |
-// +----------------------------------------------------------------------+
-//
-// $Id$
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
- * File::Gettext::PO
+ * File::Gettext
  * 
- * @author      Michael Wallner <mike@php.net>
- * @license     PHP License
- * @package     File_Gettext
- * @category    FileFormats
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   FileFormats
+ * @package    File_Gettext
+ * @author     Michael Wallner <mike@php.net>
+ * @copyright  2004-2005 Michael Wallner
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/File_Gettext
  */
 
+/**
+ * Requires File_Gettext
+ */
 require_once 'File/Gettext.php';
 
 /** 
